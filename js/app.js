28047ba10d42ff565cffd05d1348df9ea3584765
Okubanjoadedeji <okubanjoadedeji39@gmail.com>
2023-11-21 14:55:33 +0100

    new commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Logic and Control Flow
+
+// The "while" Loop
+let i = 0;
+
+while (i < 10) {
+  console.log(i);
+  i++;
+}
+
+// The "for" Loop
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
